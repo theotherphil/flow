c5e5f539184a5f8048d6a50519e845dd0cb06df3
Phil Ellison <phil.j.ellison@gmail.com>
2016-06-25 15:25:45 +0100

    Initial commit

---
+++ examples/render.rs
@@ -0,0 +1,35 @@
+
+extern crate flow;
+extern crate petgraph;
+
+use petgraph::graph::Graph;
+use petgraph::dot::Dot;
+
+fn main() {
+    // All arrows going from top to bottom, except B -> C
+    // which is left to right.
+    //
+    //          A
+    //      7      2
+    //  B       2      C
+    //      1      5
+    //          D
+
+    let mut g = Graph::<&str, f32>::new();
+    let a = g.add_node("A");
+    let b = g.add_node("B");
+    let c = g.add_node("C");
+    let d = g.add_node("D");
+
+    g.extend_with_edges(&[
+        (a, b, 7f32),
+        (a, c, 2f32),
+        (b, c, 2f32),
+        (b, d, 1f32),
+        (c, d, 5f32)
+    ]);
+
+    let dot = Dot::new(&g);
+
+    println!("{}", dot);
+}
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+
+pub fn blam(x: i32) -> i32 {
+    x + 7
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
